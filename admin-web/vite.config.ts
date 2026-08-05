1e1b2aad47b57e37ec1c7ae1175a2cda7da2ed7c
Jinghao Hu <731409213@qq.com>
2026-08-05 08:39:20 +0800

    Support admin deployment under subpath

--- admin-web/vite.config.ts
+++ admin-web/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -20,4 +21,3 @@ export default defineConfig({
     }
   }
 })
-
